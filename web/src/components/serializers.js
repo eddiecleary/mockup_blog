2f19af1a188f9ec2240c73f91b7a3e39be51c40f
eddiecleary <eddie@eddiecleary.com>
2020-09-22 17:32:57 +0800

    build bugs

--- web/src/components/serializers.js
+++ web/src/components/serializers.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import Figure from './figure.js'
 
 const serializers = {
   types: {
     authorReference: ({node}) => <span>{node.author.name}</span>,
-    mainImage: Figure
   }
 }
 
